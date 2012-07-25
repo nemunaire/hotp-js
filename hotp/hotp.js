f04bbaab21faa0f2d0cea26f03095dc78bd7ad33
Némunaire <nemunaire@pomail.fr>
2012-07-25 05:43:44 +0200

    Using CryptoJS 3 Take the raw key as argument instead of hex one Output format is no more required (dec6 by default)

--- hotp/hotp.js
+++ hotp/hotp.js
@@ -1,23 +1,33 @@
 /*
-  A simple Javascript HOTP implementation (HMAC-Based One-Time Password Algorithm) as described in RFC 4226. 
- 
-  The library is relying on crypto-js (http://code.google.com/p/crypto-js/) for the javascript HMAC-SHA1 implementation. 
- 
-  The library can be used to create software token (don't forget to protect the key of the token...).
- 
-  If you want to use the library, you'll need to load the crypto-js (sha1 and hmac) and  hotp.js.
+  A simple Javascript HOTP implementation (HMAC-Based One-Time
+  Password Algorithm) as described in RFC 4226.
 
-  Calling the library is easy, you just have to set the hex key of the token, the counter plus the output format.
+  The library is relying on crypto-js
+  (http://code.google.com/p/crypto-js/) for the javascript HMAC-SHA1
+  implementation.
 
-     otp = hotp("3132333435363738393031323334353637383930","4","dec6");
+  The library can be used to create software token (don't forget to
+  protect the key of the token...).
 
-  Current output formats are : hex40 (format used by ootp, a free software library) and dec6 (the 6 decimal digit as described in the RFC 4226).
+  If you want to use the library, you'll need to load the crypto-js
+  (sha1 and hmac) and hotp.js.
 
-  A demo page with the test vector of the RFC 4226 : http://www.foo.be/hotp/example.html*
+  Calling the library is easy, you just have to set the raw key of the
+  token, the counter plus the output format.
+
+     otp = hotp("12345678901234567890","4","dec6");
+
+  Current output formats are : hex40 (format used by ootp, a free
+  software library) and dec6 (the 6 decimal digit as described in the
+  RFC 4226).
+
+  A demo page with the test vector of the RFC 4226 :
+  http://www.foo.be/hotp/example.html*
 
   http://www.gitorious.org/hotp-js/
-  
+
   Copyright (C) 2009 Alexandre Dulaunoy
+                2012 Pierre-Olivier Mercier
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU Affero General Public License as
@@ -34,51 +44,28 @@
 
 */
 
-function hotp(key, counter, format) {
-
-    function hotp_hexkeytobytestream(s) {
-        // s is the key to be converted in bytes
-        var b = new Array();
-        var last = s.length;
-        for (var i = 0; i < last; i = i + 2) {
-            var x = s[i] + s[i + 1];
-            x.toUpperCase();
-            x = "0x" + x;
-            x = parseInt(x);
-            b[i] = String.fromCharCode(x);
-        }
-        var ret = new String();
-        ret = b.join('');
-        return ret;
-
-    }
-    function hotp_movingfactortohex(count) {
-        // count is the moving factor in OTP to be converted in bytes
-        v = decimaltohex(count, 16);
-        var decb = new Array();
-        lhex = Crypto.util.hexToBytes(v);
-        for (var i = 0; i < lhex.length; i++) {
-            decb[i] = String.fromCharCode(lhex[i]);
-        }
-        var retval = new String();
-        retval = decb.join('');
-        return retval;
-    }
-
-    function decimaltohex(d, padding) {
+function hotp(key, counter, format)
+{
+    function decimaltohex(d, padding)
+    {
         // d is the decimal value
         // padding is the padding to apply (O pad)
         var hex = Number(d).toString(16);
         padding = typeof(padding) === "undefined" || padding === null ? padding = 2 : padding;
-        while (hex.length < padding) {
+        while (hex.length < padding)
             hex = "0" + hex;
-        }
+
         return hex;
     }
 
-    function truncatedvalue(h, p) {
-        // h is the hash value
+    function truncatedvalue(ha, p)
+    {
+        // ha is the hash value
         // p is precision
+        h = [];
+        for (var i = 0; i < ha.sigBytes; i++)
+            h.push((ha.words[i >>> 2] >>> (24 - (i % 4) * 8)) & 0xff);
+
         offset = h[19] & 0xf;
         v = (h[offset] & 0x7f) << 24 | (h[offset + 1] & 0xff) << 16 | (h[offset + 2] & 0xff) << 8 | (h[offset + 3] & 0xff);
         v = "" + v;
@@ -86,19 +73,19 @@ function hotp(key, counter, format) {
         return v;
     }
 
-    var hmacBytes = Crypto.HMAC(Crypto.SHA1, Crypto.charenc.Binary.stringToBytes((hotp_movingfactortohex(counter))), Crypto.charenc.Binary.stringToBytes(hotp_hexkeytobytestream(key)));
+    var hmacBytes = CryptoJS.HmacSHA1(
+        CryptoJS.enc.Hex.parse(decimaltohex(counter, 16)),
+        CryptoJS.enc.Latin1.parse(key)
+    );
 
-    if (format == "hex40") {
-        return hmacBytes.substring(0, 10);
-    } else if (format == "dec6") {
-        return truncatedvalue(Crypto.util.hexToBytes(hmacBytes), 6);
-    } else if (format == "dec7") {
-        return truncatedvalue(Crypto.util.hexToBytes(hmacBytes), 7);
-    } else if (format == "dec8") {
-        return truncatedvalue(Crypto.util.hexToBytes(hmacBytes), 8);
-    }
-    else {
+    if (format == "hex40")
+        return hmacBytes.toString(CryptoJS.enc.Hex).substring(0, 10);
+    else if (format == "dec6" || typeof(format) === "undefined")
+        return truncatedvalue(hmacBytes, 6);
+    else if (format == "dec7")
+        return truncatedvalue(hmacBytes, 7);
+    else if (format == "dec8")
+        return truncatedvalue(hmacBytes, 8);
+    else
         return "unknown format";
-    }
-
 }
